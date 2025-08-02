1f871b93c836d5ea1221947cd9b0004735736ce8
ChenithaNethvin <165175459+Cheni-SllitA@users.noreply.github.com>
2025-08-02 15:34:05 +0530

    initial build commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-website/', // 👈 important for GitHub Pages
 })
+// This configuration sets up Vite for a React project and specifies the base path for deployment on GitHub Pages.
