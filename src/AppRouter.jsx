9a6974f5a122bb4a5c1aa95430fa3167e47867b7
ChenithaNethvin <165175459+Cheni-SllitA@users.noreply.github.com>
2025-08-03 13:51:36 +0530

    router fix

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,21 +1,18 @@
-// src/Router.jsx
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import components
 import Home from "./assets/components/Home";
 import Navbar from "./assets/components/Navbar";
 import Footer from "./assets/components/Footer";
 
 function AppRouter() {
   return (
-    <Router basename="/my-website">
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
