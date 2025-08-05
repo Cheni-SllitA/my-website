ff1fa836db7d5fc726d5bd0b27bbd651dbf36117
ChenithaNethvin <165175459+Cheni-SllitA@users.noreply.github.com>
2025-08-05 21:20:14 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/my-website/",
   plugins: [react()],
-  base: '/my-website/', // This is already correct
-})
+});
+
