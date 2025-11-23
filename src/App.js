892a7e41da5fa02a739dc332a8e8f756b0698553
ChenithaNethvin <165175459+Cheni-SllitA@users.noreply.github.com>
2025-11-23 11:28:41 +0530

    Added probity project

--- src/App.js
+++ src/App.js
@@ -32,28 +32,35 @@ export default function App() {
       description:
         "A modern online marketplace to buy vehicle accessorioes for a pleasent driving experience.",
       link: "https://github.com/Cheni-SllitA/Driivefresh",
-      // image: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?w=800",
+      image: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?w=800",
     },
     {
       title: "Rajapakse Phaeramcy web application",
       description:
-        "An admin dashboard with product management and analytics built using the MERN stack.",
-      link: "https://github.com/Ellio-Dredd/Snack-Overflow",
-      //image: "https://images.unsplash.com/photo-1556741533-f6acd6479e9c?w=800",
+        "An online web application with an admin dashboard with product management and analytics built using the MERN stack.",
+      link: "https://rajapaksepharmacy.store/",
+      image: "https://images.unsplash.com/photo-1556741533-f6acd6479e9c?w=800",
     },
     {
       title: "Rainy",
       description:
         "A real-time chat app with Socket.io, Node.js, and React supporting multiple rooms.",
       link: "https://github.com/Cheni-SllitA/Rainy-the-weather-app",
-      //image: "https://images.unsplash.com/photo-1525182008055-f88b95ff7980?w=800",
+      image: "https://images.unsplash.com/photo-1525182008055-f88b95ff7980?w=800",
     },
     {
       title: "Weather Forecast App",
       description:
         "A weather forecasting app that fetches real-time weather data using the OpenWeather API.",
       link: "https://github.com/yourusername/weather-app",
-      //image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=800",
+      image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=800",
+    },
+    {
+      title: "Probity accountants",
+      description:
+        "An online portfolio for the client's business.",
+      link: "https://www.probityaccountants.com/",
+      image: "../assets/probity.png",
     },
   ];
 
@@ -152,11 +159,13 @@ export default function App() {
             "React",
             "Node.js",
             "Java",
-            "Python",
             "Tailwind CSS",
             "MongoDB",
             "MySQL",
             "Git",
+            "HTML & CSS",
+            "javaScript",
+            "Express.js",
           ].map((skill) => (
             <motion.div
               key={skill}
@@ -274,7 +283,7 @@ function TiltCard({ project }) {
         rel="noreferrer"
         className="text-cyan-500 dark:text-cyan-400 hover:underline"
       >
-        View on GitHub →
+        View →
       </a>
     </motion.div>
   );
